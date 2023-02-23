842b5df9489bba010f6910bfb4a2c91ee6b2e6a8
형민우 <hminwo98@naver.com>
2023-02-23 17:31:15 +0900

    20230223 - test update

--- ui/test.js
+++ ui/test.js
@@ -1,155 +1,3 @@
-class Animation {
-    constructor(tg) {
-        this.el = tg
-
-        this.set()
-        this.bindEvent()
-    }
-
-    set() {
-        this.data = {}
-        this.rect = {}
-        this.isAnimated = {}
-        this.animationCount = {}
-        this.reset = []
-    }
-
-    bindEvent() {
-        this.el.forEach((aniEl, index) => {
-            window.addEventListener('load', e => {
-                this.animationAttrData(aniEl)
-                this.animationElSet(aniEl)
-            })
-            window.addEventListener('scroll', e => {
-                this.animationAttrData(aniEl)
-                this.detectAnimation(aniEl)
-                this.animationEvent(aniEl, index)
-                this.animationOnce(aniEl, index)
-            })
-        });
-    }
-
-    animationAttrData(tg) {
-        let tgAttr = tg.getAttribute('data-animation').replace(/\s*/g, '')
-        this.data = JSON.parse(tgAttr)
-    }
-
-    detectAnimation(tg) {
-        this.rect = tg.getBoundingClientRect()
-        let refRect = this.data.reference ? document.querySelector(this.data.reference).getBoundingClientRect() : undefined
-        console.log(refRect)
-
-        if (
-            !this.data.reference &&
-            !this.data.offset &&
-            this.rect.bottom >= (innerHeight * 0.2) &&
-            this.rect.top <= (innerHeight * 0.7)
-            ) this.isAnimated = true
-        else if (
-            !this.data.reference &&
-            this.data.offset &&
-            this.rect.bottom >= this.data.offset &&
-            this.rect.top <= (innerHeight - this.data.offset)
-            ) this.isAnimated = true
-        else if (
-            this.data.reference &&
-            this.data.offset &&
-            refRect.top >= this.data.offset && 
-            refRect.top <= (innerHeight - this.data.offset)
-            ) this.isAnimated = true
-        else this.isAnimated = false
-    }
-
-    animationOnce(tg, idx) {
-        if (!this.data.once && this.data.once !== undefined) {
-            let tgRect = tg.getBoundingClientRect()
-            if (tgRect.bottom <= 0 || tgRect.top >= innerHeight) {
-                this.animationElSet(tg)
-                this.animationCount[idx] = false
-                tg.classList.remove('animated')
-            }
-        }
-    }
-
-    animationElSet(tg) {
-        if (this.data.from) {
-            gsap.set(tg, ...[this.data.from])
-        } else if(!this.data.from && this.reset.length > 0) {
-            console.log(this.reset)
-            this.reset[0].pause(0)
-
-        }
-    }
-
-    animationEvent(tg, idx) {
-        if (this.isAnimated && !(tg.classList.contains('animated'))) {
-            if (this.animationCount[idx]) return false
-            this.data.to['onStart'] = () => this.animationCount[idx] = true
-            this.data.to['onComplete'] = () => tg.classList.add('animated')
-            let tween = gsap.to(tg, this.data.duration, ...[this.data.to])
-            if (this.data.once === false && !this.data.from && this.data.to) {
-                this.reset.shift()
-                this.reset.push(tween)
-            }
-        }
-    }
-}
-
-class Parallax {
-    constructor(tg) {
-        this.set()
-        this.bind(tg)
-    }
-    
-    set() {
-        this.tg = []
-        this.data = {}
-    }
-
-    bind(tg) {
-        tg.forEach((el, index) => {
-            this.parallaxData(el, index)
-            this.parallaxEvent(index)
-            this.parallaxSet(index)
-        })
-    }
-
-    parallaxData(el, idx) {
-        let data = el.getAttribute('data-parallax').replace(/\s*/g, '')
-        let datas = JSON.parse(data)
-        this.data[idx] = datas
-        this.data[idx].animate = false
-        this.data[idx].enter = datas.enter >= 0 ? datas.enter : 0
-        this.data[idx].leave = datas.leave >= 0 ? datas.leave : 100
-        this.data[idx].limit = true
-
-        this.tg.push({
-            $el: el,
-            $reference: this.data[idx].reference === undefined ? el : this.data[idx].reference,
-            data: this.data[idx]
-        })
-    }
-
-    parallaxSet(idx) {
-        gsap.set(this.tg[idx].$el, ...[this.tg[idx].data.from])
-    }
-
-    parallaxEvent(idx) {
-        let tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: this.tg[idx].$reference,
-                start: `${this.tg[idx].data.enter}% bottom`,
-                end: `${this.tg[idx].data.leave}% top`,
-                scrub: true,
-                markers: true,
-                ease: 'none'
-            }
-        })
-        
-        tl.to(this.tg[idx].$el, ...[this.tg[idx].data.to])
-    }
-}
-
 class Ui {
     constructor(tg) {
         this.set()
@@ -166,29 +14,40 @@ class Ui {
             if (el.getAttribute('data-animation')) {
                 this.animationInfo(el)
                 this.animationLocationSet(el, index)
-                this.animationEvent(el, index)
+            } else if (el.getAttribute('data-parallax')) {
+                console.log(el)
+                this.parallaxInfo(el)
+                // this.parallaxLocationSet(el, index)
             }
-            else if (el.getAttribute('data-parallax')) this.parallaxInfo(el)
+
+            window.addEventListener('scroll', () => {
+                if (el.getAttribute('data-animation')) {
+                    this.animationEvent(el, index)
+                    this.animationOnce(el, index)
+                } else if (el.getAttribute('data-animation')) {
+                    this.parallaxEvent(el, index)
+                }
+            })
         })
     }
 
+    /* Animation */
     animationData(tg) {
         let data = {}        
         let datas = tg.getAttribute('data-animation').replace(/\s*/g, '')
         data = JSON.parse(datas)
-        data.to.delay = data.to.delay ? data.to.delay : 0.5
-        data.to.duration = data.to.duration ? data.to.duration : 1
+        data.offset = data.offset ? data.offset : 200
+        data.once = data.once ? true : data.once
         return data
     }
 
-    animationInfo(tg, idx) {
-        let data = this.animationData(tg, idx)
+    animationInfo(tg) {
+        let data = this.animationData(tg)
 
         this.Animation.push({
             $el: tg,
             $reference: data.reference ? document.querySelector(data.reference) : tg,
-            data: data,
-            rect: tg.getBoundingClientRect()
+            data: data
         })
     }
 
@@ -198,22 +57,42 @@ class Ui {
     }
 
     animationEvent(tg, idx) {
+        this.Animation[idx].rect = this.Animation[idx].$reference.getBoundingClientRect()
 
-        const tl = gsap.timeline({
-            onComplete: () => {
-                tg.classList.add('animated')
-                this.Animation[idx].animated = true
-            }
-        })
+        if (
+            !tg.classList.contains('animated') &&
+            (this.Animation[idx].animated === undefined || this.Animation[idx].animated === false) &&
+            this.Animation[idx].rect.bottom <= (innerHeight - this.Animation[idx].data.offset) &&
+            this.Animation[idx].rect.top >= this.Animation[idx].data.offset
+        ) {
+            let tl = gsap.timeline({
+                onComplete: () => {
+                    tg.classList.add('animated')
+                    this.Animation[idx].animated = true
+                }
+            })
 
-        if (this.Animation[idx].data.offset && this.Animation[idx].data.reference) {
-            if (this.Animation[idx].rect.top <= (innerHeight - this.Animation[idx].rect.height) && this.Animation[idx].rect.bottom >= this.Animation[idx].rect.height) {
+            if (this.Animation[idx].data.offset && this.Animation[idx].data.reference) 
                 tl.to(tg, this.Animation[idx].data.duration, ...[this.Animation[idx].data.to])
-            }
+            else if (this.Animation[idx].data.offset && !this.Animation[idx].data.reference)
+                tl.to(tg, this.Animation[idx].data.duration, ...[this.Animation[idx].data.to])
+            else
+                tl.to(tg, this.Animation[idx].data.duration, ...[this.Animation[idx].data.to])
         }
-        
     }
 
+    animationOnce(tg, idx) {
+        if (
+            this.Animation[idx].data.once === false &&
+            (this.Animation[idx].rect.bottom <= 0 || this.Animation[idx].rect.top >= innerHeight)
+        ) {
+            this.animationLocationSet(tg, idx)
+            tg.classList.remove('animated')
+            this.Animation[idx].animated = false
+        }
+    }
+
+    /* Parallax */
     parallaxData(tg) {
         let data = {}
         let datas = tg.getAttribute('data-parallax').replace(/\s*/g, '')
@@ -234,5 +113,24 @@ class Ui {
             $reference: data.reference ? document.querySelector(data.reference) : tg,
             data: data
         })
+    }
+
+    parallaxLocationSet(tg, idx) {
+        if (this.Parallax[idx].data.from) gsap.set(tg, ...[this.Parallax[idx].data.from])
+        else return
+    }
+
+    parallaxEvent(tg, idx) {
+        let parallaxTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: tg,
+                start: 'top bottom',
+                end: 'bottom top',
+                markers: true,
+                scrub: true
+            }
+        })
+
+        parallaxTl.to(tg, ...[this.Parallax.data.to])
     }
 }
